1f2e62f6d73aeeccf790d112786cfd9b6313dbb3
Barbara-Mase <mase.barbara@gmail.com>
2026-02-18 09:58:03 +0000

    Add de l'étape 3 : récupérer un utilisateur

---
+++ get_single_user.php
@@ -0,0 +1,20 @@
+<?php
+
+require "connexion.php";
+
+require "connexion.php";
+
+$query = $db->prepare('SELECT * FROM users WHERE id = :id');
+
+$parameters = [
+    'id' => $_GET['id']
+];
+
+$query -> execute($parameters);
+
+$user = $query->fetch(PDO::FETCH_ASSOC);
+
+var_dump($user);
+
+
+?>
